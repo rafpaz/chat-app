ff6277feedcc8b9099062e37c966cf01b501c201
Refael <refaelypaz@gmail.com>
2018-06-09 22:36:31 +0300

    css improvements

--- Frontend/chat-client/src/components/MessageRow.js
+++ Frontend/chat-client/src/components/MessageRow.js
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types';
 class MessageRow extends Component {
     render() {
         return (
-            <div className={"message-row mt-3 mb-3"}>
-                <div className={"col-12 "  + (this.props.user === "You" ? "you-class" : "")}>
+            <div className={"message-row mt-3 mb-3 " + (this.props.user === "You" ? "you-class" : "")}>
+                <div className={"col-12"}>
                     <div className="row grey">{this.props.user}</div>
                     <div className="row message-text">{this.props.text}</div>
                     <div className="row grey">{this.props.date}</div>
